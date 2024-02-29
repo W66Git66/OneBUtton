ceace7be02c83a83ffaad276f6a4464c1863140b
wwx <2120405615@qq.com>
2024-02-29 13:18:46 +0800

    [second]

--- Assets/Scripts/Joystick.cs
+++ Assets/Scripts/Joystick.cs
@@ -5,11 +5,13 @@ using UnityEngine.UI;
 public class Joystick : ScrollRect
 {
     protected float JoystickRadiu = 0f;//“£∏À∑∂Œß∞Îæ∂
+    public Vector2 moveVector=new Vector2();//“°∏Àœ‡∂‘“∆∂Øæ‡¿Î
     protected override void Start()
     {
         base.Start();//‘≠start
+
         //º∆À„“°∏À∑∂Œß∞Îæ∂
-        JoystickRadiu=(transform as RectTransform).sizeDelta.x+0.45f;
+        JoystickRadiu =(transform as RectTransform).sizeDelta.x+0.45f;
     }
 
     public override void OnDrag(PointerEventData eventData)
@@ -17,12 +19,18 @@ public class Joystick : ScrollRect
         base.OnDrag(eventData);
 
         var JoystickPosition = this.content.anchoredPosition;
+        var position = this.content.localPosition;
 
         if(JoystickPosition.magnitude>JoystickRadiu)
         {
             JoystickPosition=JoystickPosition.normalized*JoystickRadiu;
             SetContentAnchoredPosition(JoystickPosition);
         }
+    }
+
+    private void Update()
+    {
+        moveVector = this.content.localPosition;//“°∏Àœ‡∂‘Œª“∆
     }
 
 
